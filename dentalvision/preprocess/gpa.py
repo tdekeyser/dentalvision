b47751f9f414a42da46c1a7116a655c2bd66ed06
tom <tom_dekeyser_7@hotmail.com>
2016-04-17 10:48:17 +0200

    reorganised

--- dentalvision/preprocess/gpa.py
+++ dentalvision/preprocess/gpa.py
@@ -6,37 +6,7 @@ for a summary of (generalised) procrustes analysis.
 
 @author: Tina Smets, Tom De Keyser
 '''
-import os
 import numpy as np
-import matplotlib.pyplot as plt
-
-
-def main():
-    path = '../Project Data/_Data/Landmarks/original/'
-    # find all shapes in target directory
-    shapes = [np.array(load(path + s)) for s in os.listdir(path) if s.endswith('.txt')]
-    
-    # perform gpa
-    mean, aligned = gpa(shapes)
-    print 'mean', mean
-    
-    plt.plot(mean[0,:], mean[1,:])
-    for a in aligned:
-        plt.plot(a[0,:], a[1,:])
-
-    axes = plt.gca()
-    axes.set_xlim([-1,1])
-    plt.show()
-
-
-def load(path):
-    '''
-    load and parse the data, and return arrays of x and y coordinates
-    '''
-    data = np.loadtxt(path)
-    x = data[::2,]
-    y = data[1::2,]
-    return x, y
 
 
 def gpa(shapes):
@@ -144,8 +114,4 @@ class ShapeAligner(object):
         # multiply VU' with shape to get the rotation
         vu = np.transpose(v).dot(np.transpose(u))
         return vu.dot(shape)
-
-
-if __name__ == '__main__':
-    main()   
     
